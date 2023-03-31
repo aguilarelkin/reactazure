f9d3b74c7220446afec438fda71ac8b7640230ca
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-03-31 17:22:49 -0500

    react-mercado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Create from './components/Create';
+import Home from './components/Home';
+import { BrowserRouter, Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { isAutenticated, hasRole } from './auth/auth.authenticated';
+import { Protected } from './interceptor/Protected';
+
 
 function App() {
+  const [user, setUser] = useState("");
+  const [sesion, setSesion] = useState(false);
+  const [roles, setRoles] = useState([]);
+  //const history = useNavigate();
+  let _token;
+  //56:
+
+  useEffect(
+    () => {
+      setSesion(isAutenticated());
+      //  getRoles();
+    }, []
+  );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      {//console.log(" ad " + sesion)
+      }
+      {//console.log(roles.includes('ROLE_ADMIN', 'ROLE_USER'))
+      }
+
+      < Routes>
+
+        <Route path='/' element={
+          <Protected sesion={sesion} redirectTo="/">
+            <Login />
+          </Protected>
+        } />
+
+        <Route path='/create/:dato' element={
+          <Protected sesion={sesion} user={['ROLE_ADMIN']} redirectTo="/">
+            <Create />
+          </Protected>} />
+
+        <Route path='/home' element={
+          <Protected sesion={sesion} user={['ROLE_ADMIN', 'ROLE_USER']} redirectTo="/">
+            <Home />
+          </Protected>
+        } />
+
+        <Route path='/home/page/:page' element={
+          <Protected sesion={sesion} user={['ROLE_ADMIN', 'ROLE_USER']} redirectTo="/">
+            <Home />
+          </Protected>
+        } />
+
+      </Routes>
+    </BrowserRouter>
+
   );
 }
+
+
 
 export default App;
